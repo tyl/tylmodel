71aadbe72f1d3ddd1572700110aa4e81b1f58a51
Marco Pancotti <mpancotti@mate.it>
2014-09-16 18:46:37 +0200

    Created TylContext

--- src/main/java/it/tylframework/data/mongo/basics/Country.kt
+++ src/main/java/it/tylframework/data/mongo/basics/Country.kt
@@ -4,6 +4,7 @@ import it.tylframework.data.mongo.common.Footprint
 import it.tylframework.data.mongo.common.Signature
 import java.util.Date
 import org.springframework.data.annotation.TypeAlias
+import it.tylframework.data.mongo.config.TylContext
 
 /**
  * Created by marco on 25/08/14.
@@ -13,5 +14,5 @@ data class Country(
         var twoCharCode: String,
         var officialName:String,
         var numericCode:Int)
-        :Footprint(Signature("marco"), Date() ,Signature("marco"),  Date()) {
+        :Footprint(TylContext.currentUser, Date() ,TylContext.currentUser,  Date()) {
 }
