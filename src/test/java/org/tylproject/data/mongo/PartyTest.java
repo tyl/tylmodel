74a2630d9d9d885e1bd7afe6eb0eec3bea14772e
Marco Pancotti <mpancotti@mate.it>
2015-01-05 23:14:53 +0100

    started Tests on Party

--- src/test/java/org/tylproject/data/mongo/PartyTest.java
+++ src/test/java/org/tylproject/data/mongo/PartyTest.java
@@ -7,12 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.tylproject.data.mongo.basics.repository.PartyRepository;
-import org.tylproject.data.mongo.party.Party;
-import org.tylproject.data.mongo.party.PartyIdentifier;
-import org.tylproject.data.mongo.party.PartyIdentifierType;
-import org.tylproject.data.mongo.party.PartyRelationshipType;
+import org.tylproject.data.mongo.party.repository.PartyRepository;
+import org.tylproject.data.mongo.common.MlText;
+import org.tylproject.data.mongo.party.*;
+import org.tylproject.data.mongo.party.repository.PartyRelationshipRepository;
 import org.tylproject.data.mongo.party.repository.PartyRelationshipTypeRepository;
+import org.tylproject.data.mongo.party.repository.PartyRoleRepository;
+import org.tylproject.data.mongo.party.repository.PartyRoleTypeRepository;
 
 import static org.junit.Assert.assertTrue;
 
@@ -31,6 +32,16 @@ public class PartyTest {
 
     @Autowired
     private PartyRelationshipTypeRepository partyRelationshipTypeRep;
+
+    @Autowired
+    private PartyRoleTypeRepository partyRoleTypeRep;
+
+    @Autowired
+    private PartyRelationshipRepository partyRelationshipRep;
+
+    @Autowired
+    private PartyRoleRepository partyRoleRep;
+
 
     @Before
     public void init() {
@@ -58,25 +69,46 @@ public class PartyTest {
         PartyRelationshipType prt02 = new PartyRelationshipType("prt02");
         mongoTemplate.save(prt02);
     }
+
+    @Test
+    public void testPartyRelationship(){
+        partyRelationshipTypeRep.deleteAll();
+        partyRoleTypeRep.deleteAll();
+        partyRelationshipRep.deleteAll();
+        partyRoleRep.deleteAll();
+        partyRep.deleteAll();
+
+
+        PartyRelationshipType prt01=new PartyRelationshipType("prt02");
+        prt01.setDescription(new MlText("prt01description"));
+        mongoTemplate.save(prt01);
+        PartyRoleType proletype01 = new PartyRoleType("prolet01",new MlText("partyroletype01"));
+        PartyRoleType proletype02 = new PartyRoleType("prolet02",new MlText("partyroletype02"));
+        mongoTemplate.save(proletype01);
+        mongoTemplate.save(proletype02);
+
+        Party party01= new Party();
+        party01.setCode("party01");
+        mongoTemplate.save(party01);
+        Party party02 = new Party();
+        party02.setCode("party02");
+        mongoTemplate.save(party02);
+
+        PartyRole partyRole01 = new PartyRole(party01,proletype01);
+        partyRole01.setName(new MlText("role01name"));
+        PartyRole partyRole02 = new PartyRole(party02,proletype02);
+        partyRole02.setName(new MlText("role02name"));
+        mongoTemplate.save(partyRole01);
+        mongoTemplate.save(partyRole02);
+
+        PartyRelationship pr = new PartyRelationship();
+        pr.setParty_relationship_type(prt01);
+        pr.setLeft_party_role(partyRole01);
+        pr.setRight_party_role(partyRole02);
+        pr.setLeft_party_code(partyRole01.getName().get());
+        pr.setRight_party_code(partyRole02.getName().get());
+        mongoTemplate.save(pr);
+    }
 }
 
-        /*
-        private Boolean freezed;
 
-    private FreezeReason freeze_reason;
-    private boolean canceled;
-
-    @Indexed
-    private String code;
-
-    @Indexed
-    private String short_name;
-
-    private ArrayList<GeographicAddress> geografic_address=new ArrayList<GeographicAddress>();
-    private ArrayList<TelecomAddress> telecom_address=new ArrayList<TelecomAddress>();
-    private ArrayList<WebAddress> web_address=new ArrayList<WebAddress>();
-    private ArrayList<EmailAddress> email_address=new ArrayList<EmailAddress>();
-
-    @Indexed
-    private ArrayList<PartyIdentifier> party_identifier=new ArrayList<PartyIdentifier>();
-         */
